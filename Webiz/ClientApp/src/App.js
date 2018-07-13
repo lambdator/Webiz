c2a5479f1f6b702ab3413f5f4db1f51e112b0300
lambdator <lambdator.developer@users.noreply.github.com>
2018-07-13 18:32:29 +0800

    DevTools Added

--- Webiz/ClientApp/src/App.js
+++ Webiz/ClientApp/src/App.js
@@ -1,14 +1,5 @@
-﻿import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
-
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+﻿if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./App.prod')
+} else {
+  module.exports = require('./App.dev')
+}
---
+++ Webiz/ClientApp/src/store/configureStore.dev.js
@@ -0,0 +1,39 @@
+﻿import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger'
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import DevTools from '../components/DevTools'
+import * as Counter from './Counter';
+import * as WeatherForecasts from './WeatherForecasts';
+
+export default function configureStore(history, initialState) {
+  const reducers = {
+    counter: Counter.reducer,
+    weatherForecasts: WeatherForecasts.reducer
+  };
+
+  const middleware = [
+    thunk,
+    createLogger(),
+    routerMiddleware(history)
+  ];
+
+  // In development, use the browser's Redux dev tools extension if installed
+  const enhancers = [];
+  enhancers.push(DevTools.instrument());
+  //const isDevelopment = process.env.NODE_ENV === 'development';
+  //if (isDevelopment && typeof window !== 'undefined' && window.devToolsExtension) {
+  //  enhancers.push(window.devToolsExtension());
+  //}
+
+  const rootReducer = combineReducers({
+    ...reducers,
+    routing: routerReducer
+  });
+
+  return createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware), ...enhancers)
+  );
+}
